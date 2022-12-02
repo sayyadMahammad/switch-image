447f72a5be7a4d69c4441502f28fe7b13b876359
sayyadMahammad <sayyadmd62847@gmail.com>
2022-12-02 12:13:27 +0530

    initial message for app distribution try no. 01

--- app/src/main/java/com/example/twopicsapp/MainActivity.kt
+++ app/src/main/java/com/example/twopicsapp/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.twopicsapp
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.microsoft.appcenter.AppCenter
-import com.microsoft.appcenter.analytics.Analytics
-import com.microsoft.appcenter.crashes.Crashes
+//
+//import com.microsoft.appcenter.AppCenter
+//import com.microsoft.appcenter.analytics.Analytics
+//import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     lateinit var pic1 : ImageView
@@ -14,10 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        AppCenter.start(
-            application, "72bf5f02-43fb-4073-a6d9-83e000ce0fcb",
-            Analytics::class.java, Crashes::class.java
-        )
+//        AppCenter.start(
+//            application, "72bf5f02-43fb-4073-a6d9-83e000ce0fcb",
+//            Analytics::class.java, Crashes::class.java
+//        )
 
         pic1=findViewById(R.id.imageView)
         pic2= findViewById(R.id.imageView2)
